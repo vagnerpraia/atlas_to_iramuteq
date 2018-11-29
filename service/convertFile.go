d23962089f442913d365a77128484731d506e30c
vagnerpraia <vagnerpraia@gmail.com>
2018-11-29 17:28:02 -0200

    Tradução da leitura do arquivo da entrevista

--- service/convertFile.go
+++ service/convertFile.go
@@ -42,15 +42,11 @@ func readFile(pathFile string) {
 		version = 7
 	}
 
-	fmt.Println(version)
-
 	if version == 7 {
 		lines = lines[15:]
 	}
 
-	fmt.Println(lines[0])
-	
-	flagHead := true
+	interviewMap := make(map[string][]string)
 	var keyInterview string
 	for index, line := range lines {
 		
@@ -62,41 +58,17 @@ func readFile(pathFile string) {
 			flagRegexHead = strings.Constains("   (Super)", line)
 		}
 
-		if flagRegexHead && flagHead {
-			keyInterview := ""
-			if version == 8 {
-				re := regexp.MustCompile(".1 [0-9]*: [0-9]*([a-zA-Z]1)?")
-				subtringRegex := re.FindStringSubmatch(line)
-			} else if version == 7 {
-				re := regexp.MustCompile(".1 [0-9]*: [0-9]*([a-zA-Z]1)?")
-				subtringRegex := re.FindStringSubmatch(line)
-				keyInterview = subtringRegex[1 : len(subtringRegex) - 2]
-			}
+		if flagRegexHead {
+			re := regexp.MustCompile(".1 [0-9]*: [0-9]*([a-zA-Z]1)?")
+			substringRegex := re.FindStringSubmatch(line)
+			substringAdjusted := substringRegex[strings.Index(":") + 2 :]
 
-			regexSearch = re.search(r' - . [0-9]*: ', row, re.M|re.I)
-			if regexSearch:
-				keyStart = regexSearch.group()
-				keyEnd = row[len(row) - 1]
-			else:
-				keyStart = ':'
-				keyEnd = ' - '
-
-			key_interview = find_between(row, keyStart, keyEnd).strip()
-			if '.' in key_interview:
-				key_interview = find_between(key_interview, '', '.').strip()
-			if re.match(r'[0-9]*e', key_interview, re.M|re.I):
-				key_interview = key_interview[:-1]
+			re = regexp.MustCompile("[0-9]*")
+			keyInterview := re.FindStringSubmatch(substringAdjusted)
+		} else if len(line) > 0 && line[0:6] != "Codes:" && line[0:8] != "No memos" {
+			interviewMap[key_interview] = append(interviewMap[key_interview], line)
 		}
-		else:
-			if row[0:6] != 'Codes:' and row[0:8] != 'No memos' and row != '':
-				if interview_dict.has_key(key_interview):
-					row_list = interview_dict.get(key_interview)
-					row_list.append(row)
-					interview_dict.update({key_interview : row_list})
-				else:
-					interview_dict.update({key_interview : [row]})
-				flagHead = True
-	*/
+	}
 }
 
 func ConvertFile(pathAtlasFile string, pathCsvFile string, pathResultFile string, separatorCsv string) bool {
