5723339931e10c91c1590335b1cd757ff1016885
vagnerpraia <vagnerpraia@gmail.com>
2018-11-29 16:49:54 -0200

    Fazendo a leitura do arquivo de questionário

--- service/convertFile.go
+++ service/convertFile.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"regexp"
 	//"reflect"
+	"strings"
 )
 
 func readFile(pathFile string) {
@@ -32,21 +33,70 @@ func readFile(pathFile string) {
 
 		lines = append(lines, line)
 	}
-	
-	flagRegex, err := regexp.MatchString("p(Report: [0-9]* quotation(s) for [0-9]* code(.*)?)ch", lines[0])
-	if err != nil {
-		log.Fatal(err)
-	}
+
+	re := regexp.MustCompile("Report: [0-9]* quotation\\(s\\) for [0-9]* code( *)?")
+	flagRegexVersion := re.MatchString(lines[0])
 
 	version := 8
-	if flagRegex {
+	if flagRegexVersion {
 		version = 7
-		fmt.Println("Version 7")
-	} else {
-		fmt.Println("Version 8")
 	}
 
 	fmt.Println(version)
+
+	if version == 7 {
+		lines = lines[15:]
+	}
+
+	fmt.Println(lines[0])
+	
+	flagHead := true
+	var keyInterview string
+	for index, line := range lines {
+		
+		flagRegexHead := true
+		if version == 8 {
+			re := regexp.MustCompile("([0-9]*):([0-9]*) .*")
+			flagRegexHead = re.MatchString(line)
+		} else if version == 7 {
+			flagRegexHead = strings.Constains("   (Super)", line)
+		}
+
+		if flagRegexHead && flagHead {
+			keyInterview := ""
+			if version == 8 {
+				re := regexp.MustCompile(".1 [0-9]*: [0-9]*([a-zA-Z]1)?")
+				subtringRegex := re.FindStringSubmatch(line)
+			} else if version == 7 {
+				re := regexp.MustCompile(".1 [0-9]*: [0-9]*([a-zA-Z]1)?")
+				subtringRegex := re.FindStringSubmatch(line)
+				keyInterview = subtringRegex[1 : len(subtringRegex) - 2]
+			}
+
+			regexSearch = re.search(r' - . [0-9]*: ', row, re.M|re.I)
+			if regexSearch:
+				keyStart = regexSearch.group()
+				keyEnd = row[len(row) - 1]
+			else:
+				keyStart = ':'
+				keyEnd = ' - '
+
+			key_interview = find_between(row, keyStart, keyEnd).strip()
+			if '.' in key_interview:
+				key_interview = find_between(key_interview, '', '.').strip()
+			if re.match(r'[0-9]*e', key_interview, re.M|re.I):
+				key_interview = key_interview[:-1]
+		}
+		else:
+			if row[0:6] != 'Codes:' and row[0:8] != 'No memos' and row != '':
+				if interview_dict.has_key(key_interview):
+					row_list = interview_dict.get(key_interview)
+					row_list.append(row)
+					interview_dict.update({key_interview : row_list})
+				else:
+					interview_dict.update({key_interview : [row]})
+				flagHead = True
+	*/
 }
 
 func ConvertFile(pathAtlasFile string, pathCsvFile string, pathResultFile string, separatorCsv string) bool {
